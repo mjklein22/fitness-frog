1bb69f64d25761a636f600b56f58605e01febdf8
Matthew Klein <mjklein22@gmail.com>
2017-05-05 06:04:39 -0500

    Updated the "Activity" Field to Use a DropDown List

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -43,8 +43,10 @@ namespace Treehouse.FitnessFrog.Controllers
         {
             var entry = new Entry()
             {
-                Date = DateTime.Today
+                Date = DateTime.Today,
             };
+
+            ViewBag.ActivitiesSelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
 
             return View(entry);
         }
@@ -59,6 +61,8 @@ namespace Treehouse.FitnessFrog.Controllers
 
                 //TODO Display the Entries List Page
             }
+
+            ViewBag.ActivitiesSelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
 
             return View(entry);
         }
